96928385946c6da6473c825d16c69ca893093638
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-04-18 19:51:00 -0300

    alterado arquivo original

--- aula2.py
+++ aula2.py
@@ -5,7 +5,9 @@ idade = input("qual sua idade:  ")
 """
 Isso e um comentario de uma linha
 """
-
+# Primeira forma de concatenar
 print ("ola",nome,"sua idade e ",idade)
 print ("ola {} sua idade e {}".format(nome, idade))
 print (f"ola {nome} sua idade e {idade}")
+
+print("Estou no arquivo original")
