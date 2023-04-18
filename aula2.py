6d3760ca86ef4a6c0e02ca9e24ca8306902c6bb4
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-04-17 21:02:26 -0300

    Criado Aula 2 em python

---
+++ aula2.py
@@ -0,0 +1,11 @@
+nome = input ("Informe seu nome: ")
+idade = input("qual sua idade:  ")
+
+# Isso e um comentario de uma linha
+"""
+Isso e um comentario de uma linha
+"""
+
+print ("ola",nome,"sua idade e ",idade)
+print ("ola {} sua idade e {}".format(nome, idade))
+print (f"ola {nome} sua idade e {idade}")
