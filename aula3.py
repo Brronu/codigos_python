cf4f59f13c6b4743fb1cc985f0240a86cdadf696
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-04-19 19:14:10 -0300

    Criado aula 3

---
+++ aula3.py
@@ -0,0 +1,6 @@
+nota1 = float(input("informe a nota 1: "))
+nota2 = float(input("informe a nota 2: "))
+nota3 = float(input("informe a nota 3: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print (f" A media final e: {media}\n")
