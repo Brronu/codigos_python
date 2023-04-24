26ec22afabc98eab1486451dd56e02e37ddb93ad
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-04-24 20:34:51 -0300

    Finalizado atividade condicional

---
+++ Atividade condicional/atv1.py
@@ -0,0 +1,6 @@
+valor = int(input("Infome um valor qualquer: "))
+
+if valor >=100 and valor <=200: 
+    print (f"o valor {valor} esta entre 100 e 200\n")
+else:
+    print(f"o valor {valor} não esta 100 e 200\n")
