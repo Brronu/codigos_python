815898385c7bad0bef4e482aa81aab4654807825
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-04-24 21:15:11 -0300

    inisiado Atividade para

---
+++ Atividade Para/Atv1.py
@@ -0,0 +1,7 @@
+s = 0
+m = 0
+t = 0
+
+for cont in range(50, 71):
+    t = t + 1
+print(f"o total e {t}\n")
---
+++ aula6-para.py
@@ -0,0 +1,4 @@
+for contador in range(1,10+1):
+    print(contador)
+for contador in range(10,1, -1): # O -indica que a contagem ira começar do maior para o menor
+    print(contador)
