620e6b5b0c6b1e37a7f0ab7ccf89d97fa1180c20
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-05-02 20:45:12 -0300

    Adicionado atividade 5

--- Atividade Para/atv5.py
+++ Atividade Para/atv5.py
@@ -2,9 +2,12 @@ valor = int(input("informe um valor qualquer: "))
 
 maior = 0
 posicao = 0
+
 for cont in range(1, valor + 1):
     item = int(input("informe um valor: "))
     if item >= maior:
         maior = item
         posicao = contador 
-         
+
+print(f"O maior valor é {maior} e esta na posição{posicao}\n") 
+
