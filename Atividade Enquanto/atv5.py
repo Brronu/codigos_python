3d607feb85ea3d6dc6a0c0111085b4b4cdafa468
Bruno Oliveira <brunooliveira.2115b@gmail.com>
2023-05-04 19:46:21 -0300

    Finalizado atividade enquanto

---
+++ Atividade Enquanto/atv5.py
@@ -0,0 +1,13 @@
+while True:
+    num = int(input("Informe um valor entre 1 e 9: "))
+    if num >= 1 and num <= 9:
+        break
+
+cont = 1
+while cont <= 10:
+    if num % 2== 0:
+        print(f"{num} x {cont} = {num * cont}")
+    else:
+        print(f"{num} + {cont} = {num + cont}")
+        
+    cont +=1
---
+++ Atividade Enquanto/atv6.py
@@ -0,0 +1,16 @@
+import os # importando bilioteca  para trabalhar com sistema operacional
+dentroIntervalo = 0
+foraIntervalo = 0
+cont = 1
+
+os.system("cls") # irá limpa a tela
+while cont <= 5:
+    valor = int(input(f"informe o valor {cont}: "))
+    if valor >=10 and valor <=20: 
+         dentroIntervalo += 1
+    else:
+        foraIntervalo += 1
+    cont += 1
+
+print(f"Valores dentro do intervalo: {dentroIntervalo}")
+print(f"Valores fora do intervalo: {foraIntervalo}")
